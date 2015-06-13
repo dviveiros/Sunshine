2f3cd289ea7f52df0e034a381cb5dd3c23ee514d
dviveiros <daniel.viveiros@gmail.com>
2015-06-13 10:45:30 -0300

    Working on layout

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.example.android.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,6 +23,26 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //forecast fake data
+        List<String> weekForecast = new ArrayList<String>();
+
+        weekForecast.add("Total - Sunny - 20 / 30");
+        weekForecast.add("Tomorrow - Cloudy - 15 / 22");
+        weekForecast.add("Mon - Sunny - 19 / 27");
+        weekForecast.add("Tue - Sunny - 28 / 25");
+        weekForecast.add("Wed - Snow - 0 / 5");
+        weekForecast.add("Thurs - Cloudy - 5 / 12");
+        weekForecast.add("Fri - Sunny - 21 / 25");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+
+        ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(adapter);
+
+        return rootView;
     }
 }
